dfb7045aaaa76a19b0165e233c45f795b23576b4
Rafael Saca <raffaellsaca@gmail.com>
2023-08-25 12:52:13 -0300

    Comportamento do INPUT  - entrada de dados e limpa campos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
-const App = () => <h1 className='title'>App</h1>
+const App = () => {
 
+const ESCAPE_KEY = 27;//tecla enter tabela asc
+const ENTER_KEY = 13;//teclas esc tabela asc
+
+const [value, setValue] = useState("");
+
+const erase = ()=>{
+    setValue('');
+}
+
+const submit = () => {
+    console.log('submit',value);
+    erase();
+}
+
+const onChange = (event) => {
+    setValue(event.target.value);
+}
+
+const onKeyDown = (event) =>{
+    if(event.which === ENTER_KEY ){
+       submit();
+
+    }else if(event.which ===  ESCAPE_KEY){
+        erase();
+    }
+}
+
+
+
+  return (
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">ToDo</h1>
+      </header>
+
+      <section className="main">
+        <input className="new-todo" placeholder="O que precisa ser feito?"
+        value={value} 
+        onChange={onChange}
+        onKeyDown={onKeyDown} />
+      </section>
+
+
+    </section>
+  );
+};
 export default App;
