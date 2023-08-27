4aa4ad5da6d21d8143b57883181b218438f43eaa
Rafael Saca <raffaellsaca@gmail.com>
2023-08-27 14:35:47 -0300

    Refatorando Listagem de tarefas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { MdDelete } from "react-icons/md";
+
 
 import NewTodo from "./components/NewTodo";
-import "./App.css";
+import TodoList from "./components/TodoList";
 
+import "./App.css";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
- 
+
   const onNewTodo = (value) => {
     setTodos([
       ...todos, //array com os elementos anteriores
@@ -18,50 +19,29 @@ const App = () => {
         checked: false,
       },
     ]);
-  }
+  };
 
   const onToggle = (todo) => {
     //RISCAR CHECAR A TAREFA
     setTodos(
       todos.map((obj) =>
-      (obj.id === todo.id ? {...obj, checked: !todo.checked } : obj))
+        obj.id === todo.id ? { ...obj, checked: !todo.checked } : obj
+      )
     );
   };
 
- const onRemove = (todo) => {
-  setTodos(todos.filter((obj) => obj.id !== todo.id));
- }
+  const onRemove = (todo) => {
+    setTodos(todos.filter((obj) => obj.id !== todo.id));
+  };
 
   return (
     <section id="app" className="container">
       <header>
         <h1 className="title">ToDo</h1>
       </header>
-
       <section className="main">
-        <NewTodo onNewTodo={ onNewTodo } />
-        
-        <ul className="todo-list">
-          {todos.map((todo) => (
-            <li key={todo.id.toString()}>
-              <span
-                className={["todo",todo.checked ? 
-                "checked" : ""].join(" ")}
-                onClick={() => onToggle(todo)}
-                onKeyPress={() => onToggle(todo)}
-                role="button"
-                tabIndex={0}
-              >
-                {todo.title}
-              </span>
-              <button className="remove" type="button"
-                onClick={()=> onRemove(todo)}
-              >
-                <MdDelete size={28} />
-              </button>
-            </li>
-          ))}
-        </ul>
+        <NewTodo onNewTodo={onNewTodo} />
+        <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
       </section>
     </section>
   );
